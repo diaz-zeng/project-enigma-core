dc54462afaaf2db2dee067d5e04c417d77334cca
曾令宇 <diaz_zeng@outlook.com>
2022-01-18 16:19:38 +0800

    feat: update doc config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,11 @@
 /*
  * @Date: 2021-11-09 16:49:17
  * @LastEditors: 曾令宇
- * @LastEditTime: 2022-01-18 12:26:06
+ * @LastEditTime: 2022-01-18 16:19:22
  * @FilePath: /project-enigma-core/docs/.vuepress/config.js
  */
 module.exports = {
-    base: "/project-enigma-core",
+    base: "/project-enigma-core/",
     title: 'Project Enigma Core', locales: {
         '/': {
             lang: 'zh-CN', // 将会被设置为 <html> 的 lang 属性
